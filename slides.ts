bdefd3b34ec6e95fb1d61efa0bc8e73e3d4209a8
Pedro Dahmer <pedrohd1213@gmail.com>
2021-07-29 16:42:08 -0300

    first commit

---
+++ slides.ts
@@ -0,0 +1,29 @@
+export const slides = [
+    {
+        id: 0,
+        image: '/images/1.jpg',
+        title: 'pedrao',
+        subtitle: 'dkjfaklsdjfklçasdlç',
+        ref: 'pedrao',
+    },
+    {
+        image: '/images/2.jpg',
+        title: 'iggie',
+        subtitle: 'kdjfaçkldjfslakdfjlçsdifeirete',
+        ref: 'iggie',
+    },
+    {
+        id: 2,
+        image: '/images/3.jpg',
+        title: 'mari',
+        subtitle: 'texto',
+        ref: 'mari',
+    },
+    {
+        id: 3,
+        image: '/images/4.jpg',
+        title: 'juliano',
+        subtitle: 'texto',
+        ref: 'juliano',
+    },
+]
